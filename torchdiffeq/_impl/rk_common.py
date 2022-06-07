c77602eecfbe4a7fa51e9c63dce5a47764625afc
veragnezdilova <76115212+veragnezdilova@users.noreply.github.com>
2022-06-07 06:07:14 +0300

    Update rk_common.py

--- torchdiffeq/_impl/rk_common.py
+++ torchdiffeq/_impl/rk_common.py
@@ -159,6 +159,7 @@ class RKAdaptiveStepsizeODESolver(AdaptiveStepsizeEventODESolver):
     def _before_integrate(self, t):
       t0 = t[0]
       f0 = self.func(t[0], self.y0)
+      first_step = _select_initial_step(self.func, t[0], self.y0, self.order - 1, self.rtol, self.atol, self.norm, f0=f0)
       self.rk_state = _RungeKuttaState(self.y0, f0, t[0], t[0], first_step, [self.y0] * 5)
       '''
         t0 = t[0]
